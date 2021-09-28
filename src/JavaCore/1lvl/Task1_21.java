a9f4c9239aa55f42d36976162b97e3de173ddada
Victor_Shumskiy <victor_shumskiy@mail.ru>
2021-09-28 21:31:00 +0300

    added Task1_21

---
+++ src/JavaCore/1lvl/Task1_21.java
@@ -0,0 +1,36 @@
+package Tasks;
+
+import java.util.Arrays;
+
+public class Task1_21 {
+	public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair result = getMinimumMaximumPair(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair getMinimumMaximumPair(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair(null, null);
+        }
+
+        int [] sortedArray = Arrays.copyOf(inputArray, inputArray.length);
+        Arrays.sort(sortedArray);
+
+        return new Pair(sortedArray[0], sortedArray[sortedArray.length - 1]);
+    }
+
+    public static class Pair {
+        public Integer x;
+        public Integer y;
+
+        public Pair(Integer x, Integer y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
